0e2852f1d6cd5efe7537a26855a00ab2eb1e5663
Santiago Calligari <carlossantiagocalligari@gmail.com>
2026-02-05 19:30:34 -0300

    Main fix

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_time::Timer;
+use embassy_rp::gpio;
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
+    let _ = embassy_rp::init(Default::default());
     info!("Hello, World");
 }
