0d29c59987ef7ab6830876ade025d6ec8e1b4205
Santiago Calligari <carlossantiagocalligari@gmail.com>
2026-02-05 19:28:26 -0300

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    info!("Hello, World");
 }
